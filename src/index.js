5809a67ba1794827d38a0247ea1c3fadabc9eecc
vladji <vladjislav@gmail.com>
2019-03-22 15:32:36 +0300

    money-exchange/feat: task is solved

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  let exchange = {};
+    if (currency <= 0) 
+    return exchange;
+    if (currency >= 10000) {
+      exchange["error"] = "You are rich, my friend! We don't have so much coins for exchange";
+      return exchange;
+    }
+  let firstElement = currency / 50 ^ 0;
+    if (firstElement !== 0)
+    exchange["H"] = firstElement;
+  currency -= (firstElement * 50);
+  let secondElement = currency / 25 ^ 0;
+    if (secondElement !== 0)
+    exchange["Q"] = secondElement;
+  currency -= (secondElement * 25);
+  let thirdElement = currency / 10 ^ 0;
+    if (thirdElement !== 0)
+    exchange["D"] = thirdElement;
+  currency -= (thirdElement * 10);
+  let fourthElement = currency / 5 ^ 0;
+    if (fourthElement !== 0)
+    exchange["N"] = fourthElement;
+  currency -= (fourthElement * 5);
+  let fifthElement = currency;
+    if (fifthElement !== 0)
+    exchange["P"] = fifthElement;
+  return exchange;
 }
